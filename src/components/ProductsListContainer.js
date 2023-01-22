0ca9f58b87ef9f5f23b4f2e8c0237e8891e29342
Cristhian Pabon <cristhian.pabon@fluxit.com.ar>
2023-01-22 01:39:39 -0300

    Importing a new file of styles with media queries changes on ProductsListContainer

--- src/components/ProductsListContainer.js
+++ src/components/ProductsListContainer.js
@@ -3,12 +3,11 @@ import { useSelector, useDispatch } from "react-redux";
 
 import ProductItem from "./ProductItem";
 
-// import "./ProductsPages.css";
+import "./ProductsListContainer.css";
 
 function ProductsListContainer() {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
-  console.log(products);
 
   return (
     <div className="productList">
