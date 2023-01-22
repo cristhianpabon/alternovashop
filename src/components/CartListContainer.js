997f3c6830751337d0f47e6c7d0d0f53a0475b06
Cristhian Pabon <cristhian.pabon@fluxit.com.ar>
2023-01-22 02:26:14 -0300

    Adding to the cart an empty state to display a message if is empty, or display a cart with a list of products

--- src/components/CartListContainer.js
+++ src/components/CartListContainer.js
@@ -1,32 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { addCartItem } from "../redux/slice/cartSlice";
+import React from "react";
+import { useSelector } from "react-redux";
 
 import CartItem from "../components/CartItem";
 
 import "./CartListContainer.css";
 
-function CartListContainer() {
-    const dispatch = useDispatch();
-    const { cart } = useSelector((state) => state.cart);
-    console.log(cart);
+const CartListContainer = () => {
+  const { cart } = useSelector((state) => state.cart);
 
   return (
-        <div className="cartList">
-          <div className="cart">
+    <div className="cartList">
+      <div className="cart">
+        {cart.length ? (
+          <>
             <div>
               <h2>CART</h2>
-              <CartItem />
-              <CartItem />
-              <CartItem />
+              {cart.map((cartItem, index) => (
+                <CartItem {...cartItem} key={index} />
+              ))}
             </div>
             <div className="cartFooter">
               <p>Total Order Price</p>
               <button>Create Order</button>
             </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <p>Your cart is empty..</p>
+        )}
+      </div>
+    </div>
   );
-}
+};
 
 export default CartListContainer;
